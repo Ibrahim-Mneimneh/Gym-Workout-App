b52af4c51e9bc43a5b3acffb21981ce8d6936f84
Ibrahim <ibrahim.mneimneh0@gmail.com>
2023-07-02 20:52:51 +0300

    Fix login issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,12 +18,12 @@ function App() {
               element={user ? <Home /> : <Navigate to="/login" />}
             />
             <Route
-              path="/login"
-              element={!user ? <Login /> : <Navigate to="/" />}
-            />
-            <Route
               path="/signup"
               element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+            <Route
+              path={"/login" || "/Login"}
+              element={!user ? <Login /> : <Navigate to="/" />}
             />
           </Routes>
         </div>
