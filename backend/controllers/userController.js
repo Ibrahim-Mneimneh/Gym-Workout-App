349e1ec59e71361028631e42f5f2bfbff22b69c5
Ibrahim <ibrahim.mneimneh0@gmail.com>
2023-07-02 17:15:47 +0300

    Connection Fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-  return jwt.sign({ _id }, "dnjdnjfn§5$§fuhek", {
+  return jwt.sign({ _id }, process.env.SECRET, {
     expiresIn: "1d",
   });
 };
